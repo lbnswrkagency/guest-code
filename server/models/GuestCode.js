97a6973344f39f44b7d5020dd56dea514a2d09f8
Zafer <zafer.gueney@gmail.com>
2023-12-04 16:43:38 +0200

    test

--- server/models/GuestCode.js
+++ server/models/GuestCode.js
@@ -1,13 +1,19 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const GuestCodeSchema = new Schema({
-  event: { type: Schema.Types.ObjectId, ref: "Event" },
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  condition: { type: String, required: true },
-  pax: { type: Number, required: true },
-  paxChecked: { type: Number, required: true },
-});
+const GuestCodeSchema = new Schema(
+  {
+    event: { type: Schema.Types.ObjectId, ref: "Event" },
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    condition: { type: String, required: true },
+    pax: { type: Number, required: true },
+    paxChecked: { type: Number, required: true },
+    paxCheckedTime: { type: Date },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("GuestCode", GuestCodeSchema);
