05e7026aa52119ab1e9f52839ec2766f7fdcd61a
Zafer <zafer.gueney@gmail.com>
2024-05-11 09:22:58 +0300

    bug fixes

--- server/controllers/codeController.js
+++ server/controllers/codeController.js
@@ -227,8 +227,10 @@ const generateCodeImage = async (req, res) => {
               <div> 
                 <div style="margin-top: 0.5rem;">
                     <p style="margin: 0; color: #A6965D; font-weight: 600; font-size: 0.625rem; line-height: 1rem;">Line Up</p>                 
-                    <p style="margin: 0; font-weight: 500; font-size: 0.857rem; color: #fff; line-height: 1.25rem;">Hulk</p>
-                    <p style="margin: 0; font-weight: 500; font-size: 0.857rem; color: #fff; line-height: 1.25rem;">Hendricks</p>
+                    <p style="margin: 0; font-weight: 500; font-size: 0.857rem;  line-height: 1.25rem;">Hulk</p>
+                    <p style="margin: 0; font-weight: 500; font-size: 0.857rem;  line-height: 1.25rem;">Hendricks</p>
+                    <p style="margin: 0; font-weight: 500; font-size: 0.857rem;  line-height: 1.25rem;">Haven Nile</p>
+                    <p style="margin: 0; font-weight: 500; font-size: 0.857rem;  line-height: 1.25rem;">Tokyo</p>
                 </div>
 
               </div>
@@ -307,7 +309,10 @@ const generateCodeImage = async (req, res) => {
                   <p style="margin: 0; color: #A6965D; font-weight: 600; font-size: 0.625rem; line-height: 1rem;">Line Up</p> 
                   <p style="margin: 0; font-weight: 500; font-size: 0.857rem; color: #fff; line-height: 1.25rem;">Hulk</p>
                   <p style="margin: 0; font-weight: 500; font-size: 0.857rem; color: #fff; line-height: 1.25rem;">Hendricks</p>
-                </div>
+                  <p style="margin: 0; font-weight: 500; font-size: 0.857rem; color: #fff; line-height: 1.25rem;">Haven Nile</p>
+                  <p style="margin: 0; font-weight: 500; font-size: 0.857rem; color: #fff; line-height: 1.25rem;">Tokyo</p>
+                
+                  </div>
               </div>
 
 
