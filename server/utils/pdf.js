7f2ddfdae7cb4d99c119a6e6e15ff1f09d7b33c0
Zafer <zafer.gueney@gmail.com>
2024-06-28 00:58:29 +0300

    x

--- server/utils/pdf.js
+++ server/utils/pdf.js
@@ -248,7 +248,7 @@ const createTicketPDF = async (
 
         <div class="event-date">
           <h3>Date</h3>
-          <p>Wednesday, 26.06.2024</p>
+          <p>Wednesday, 03.07.2024.2024</p>
         </div>
 
         <div class="event-beats">
