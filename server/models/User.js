5322a71ef05e5882189bc97e52934bda642d79bc
Zafer <zafer.gueney@gmail.com>
2024-08-15 15:48:20 +0300

    First StartAlpha Commit

--- server/models/User.js
+++ server/models/User.js
@@ -3,28 +3,25 @@ const Schema = mongoose.Schema;
 const bcrypt = require("bcryptjs");
 
 const UserSchema = new Schema({
-  firstName: { type: String, required: true },
-  email: { type: String, required: false, unique: true },
-  lastName: { type: String, required: true },
   username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
   birthday: { type: Date, required: true },
+  avatar: { type: String, default: "" },
+  createdAt: { type: Date, default: Date.now },
+  brands: [{ type: Schema.Types.ObjectId, ref: "Brand" }],
+  followedBrands: [{ type: Schema.Types.ObjectId, ref: "Brand" }],
+  eventMemberships: [
+    {
+      event: { type: Schema.Types.ObjectId, ref: "Event" },
+      roles: [String],
+      permissions: [String],
+    },
+  ],
   isVerified: { type: Boolean, default: false },
   isAdmin: { type: Boolean, default: false },
-  isScanner: { type: Boolean, default: false },
-  isPromoter: { type: Boolean, default: false },
-  isStaff: { type: Boolean, default: false },
-  isDeveloper: { type: Boolean, default: false },
-  isBackstage: { type: Boolean, default: false },
-  isTable: { type: Boolean, default: false },
-  backstageCodeLimit: { type: Number, default: false },
-  friendsCodeLimit: { type: Number, default: false },
-  createdAt: { type: Date, default: Date.now },
-  events: [{ type: Schema.Types.ObjectId, ref: "Event" }],
-  avatar: {
-    type: String,
-    default: "",
-  },
 });
 
 UserSchema.methods.comparePassword = function (candidatePassword) {
